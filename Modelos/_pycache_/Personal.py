1ec12e250916e53cee601412e038905a47f7ac48
YagoMartins77 <yagofelipeoliveira3@gmail.com>
2025-11-07 01:23:06 +0000

    Formação da tabela

---
+++ Modelos/_pycache_/Personal.py
@@ -0,0 +1,40 @@
+classclass Personal:
+    def_init_(self,cpf,rg,nome,horarios_disponiveis,telefone):
+        self._cpf = cpf
+        self._rg = rg
+        self._nome = nome
+        self._horario_disponiveis = time
+        self._telefone = telefone
+
+    def get_cpf(self):
+        return self._cpf
+
+    def set_cpf(self, cpf):
+        self._cpf = cpf
+ 
+    def get_rg(self):
+        return self._rg
+
+    def set_rg(self, rg):
+        self._rg = rg
+
+    def get_nome(self):
+        return self._nome
+
+    def set_nome(self, nome):
+        self._nome = nome
+     
+    def get_time(self):
+        return self._time
+
+    def set_time(self, time):
+        self._time = time
+     
+     def get_telefone(self):
+        return self._telefone
+
+    def set_telefone(self, telefone):
+        self._telefone = telefone
+
+
+        pass
