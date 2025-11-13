fc78aeff5ecdb7cf79b00172ee540d6efecc6581
YagoMartins77 <yagofelipeoliveira3@gmail.com>
2025-11-12 21:43:26 -0300

    Update Aula.py

---
+++ Modelos/Aula.py
@@ -0,0 +1,29 @@
+class Alula:
+    def_init_(self, id_aula, tipo_aula, id_professor):
+        self._id_aula = id_aula
+        self._tipo_aula = tipo_aula
+        self._id_professor = id_professor
+
+    @property
+    def id_aula(self):
+        return self._id_aula
+
+    @id_aula.setter
+    def id_aula(self, value):
+        self._id_aula = value
+
+    @property
+    def tipo_aula(self):
+        return self._tipo_aula
+
+    @tipo_aula.setter
+    def tipo_aula(self, value):
+        self._tipo_aula = value
+
+    @property
+    def id_professor(self):
+        return self._id_professor
+
+    @id_professor.setter
+    def id_professor(self, value):
+        self._id_professor = value
