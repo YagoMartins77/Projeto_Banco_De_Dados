3032127e4e8a843dfb8523738436b834e156460e
YagoMartins77 <yagofelipeoliveira3@gmail.com>
2025-11-12 21:49:37 -0300

    Update Treino.py

---
+++ Modelos/Treino.py
@@ -0,0 +1,58 @@
+class Treino:
+    def_init_(self, id_tr, alongamentos, exercicios_arbcs, exercicios_mqn, carga_mqn, cpf_aluno):
+        self._id_tr = id_tr 
+        self._alongamentos = alongamentos
+        self._exercicios_mqn = exercicios_mqn
+        self._exercicios_arbcs = exercicios_arbcs
+        self._carga_mqn = carga_mqn
+        self._cpf_aluno = cpf_aluno
+
+    @property
+    def id_tr(self):
+        return self._id_tr
+
+    @id_tr.setter
+    def id_tr(self, value):
+        self._id_tr = value
+
+    @property
+    def alongamentos(self):
+        return self._alongamentos
+
+    @alongamentos.setter
+    def alongamentos(self, value):
+        self._alongamentos = value
+
+    @property
+    def exercicios_mqn(self):
+        return self._exercicios_mqn
+
+    @exercicios_mqn.setter
+    def exercicios_mqn(self, value):
+        self._exercicios_mqn = value
+
+    @property
+    def exercicios_arbcs(self):
+        return self._exercicios_arbcs
+
+    @exercicios_arbcs.setter
+    def exercicios_arbcs(self, value):
+        self._exercicios_arbcs = value
+
+    @property
+    def carga_mqn(self):
+        return self._carga_mqn
+
+    @carga_mqn.setter
+    def carga_mqn(self, value):
+        self._carga_mqn = value
+
+    @property
+    def cpf_aluno(self):
+        return self._cpf_aluno
+
+    @cpf_aluno.setter
+    def cpf_aluno(self, value):
+        self._cpf_aluno = value
+
+        pass
